f8730be61d417077813693d11fd34aac40a7a4a4
Singh-Arshvir <79410121+Singh-Arshvir@users.noreply.github.com>
2026-03-28 12:14:37 +0530

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-  ],
-})
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./", // ✅ ensures correct asset paths on Vercel
+});
